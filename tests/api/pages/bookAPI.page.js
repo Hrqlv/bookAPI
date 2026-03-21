d9b5c73b07b996e447b038bcbf56f5d0814e974e
Hrqlv <henriquelopesvelozo@gmail.com>
2026-03-21 12:31:58 -0300

    test: EstruturaDosTests

---
+++ tests/api/pages/bookAPI.page.js
@@ -0,0 +1,36 @@
+import { expect, request } from "@playwright/test";
+
+export class ServicesAPI {
+    constructor() {
+        this.authToken = null;
+        this.urlBase = 'https://restful-booker.herokuapp.com';
+    }
+    
+    async createToken() {
+
+    }
+
+    async getBook() {
+
+    }
+
+    async getBookID() {
+
+    }
+
+    async createBook() {
+
+    }
+
+    async updateBook() {
+
+    }
+
+    async updatePartialBook() {
+
+    }
+
+    async deleteBook() {
+
+    }
+}
