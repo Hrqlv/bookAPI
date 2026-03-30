2209afa8b38758b914fb85033088dc5832c26fca
Hrqlv <henriquelopesvelozo@gmail.com>
2026-03-30 10:24:45 -0300

    test: Ajustes no yml e config

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  timeout: 30000,
   testDir: './tests/api',
-
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
